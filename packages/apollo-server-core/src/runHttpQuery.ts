9e2f1d0e31504e288771e2f2e8939cb850804a6c
xutian17 <xu@houzz.com>
2017-11-19 23:49:20 -0800

    bug fix

--- packages/apollo-server-core/src/runHttpQuery.ts
+++ packages/apollo-server-core/src/runHttpQuery.ts
@@ -146,9 +146,11 @@ export async function runHttpQuery(handlerArguments: Array<any>, request: HttpQu
         'Content-Type': 'application/json',
       });
     }
+
     if (!optionsObject.formatResponse(null, null, true)) {
         return gqlResponse;
     }
+    
     return JSON.stringify(gqlResponse);
   }
 
